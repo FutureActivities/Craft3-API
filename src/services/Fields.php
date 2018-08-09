6a0fd7c532f71c197cacb6a3e61559277d00d573
Paul Blundell <paul@blundell.xyz>
2018-08-09 14:26:21 +0000

    Change how element query types are processed

--- src/services/Fields.php
+++ src/services/Fields.php
@@ -8,10 +8,12 @@ use futureactivities\api\Plugin;
 
 class Fields extends Component
 {
+    const MAX_LEVELS = 3;
+    
     /**
      * Processes a craft field and returns a usable value
      */
-    public function process($field)
+    public function process($field, $level = 1)
     {
         if (is_null($field) || is_string($field) || is_bool($field) || is_int($field) || is_array($field))
             return $field;
@@ -19,11 +21,8 @@ class Fields extends Component
         if (is_a($field, 'DateTime'))
             return $field->format('Y-m-d H:i:s');
         
-        if (get_class($field) === 'craft\elements\db\MatrixBlockQuery' || get_class($field) === 'craft\elements\db\TagQuery')
-            return $this->elementQuery($field);
-        
         if (get_parent_class($field) === 'craft\elements\db\ElementQuery')
-            return $field->ids();
+            return $this->elementQuery($field, $level);
         
         if (is_a($field, 'craft\fields\data\SingleOptionFieldData'))
             return $this->singleOption($field);
@@ -43,11 +42,16 @@ class Fields extends Component
     /**
      * Loops through an element query results parsing the attributes for each result
      */
-    protected function elementQuery($query)
+    protected function elementQuery($query, $level = 1)
     {
+        if ($level > self::MAX_LEVELS)
+            return 'ERROR: NESTING TOO DEEP';
+            
         $result = [];
         foreach($query->all() AS $element) {
-            $parsed = Plugin::getInstance()->helper->parseAttributes($element);
+            $parsed = [];
+            foreach($element->getAttributes() AS $key => $attribute)
+                $parsed[$key] = $this->process($attribute, $level + 1);
             
             if (isset($element->type) && isset($element->type->handle))
                 $parsed['handle'] = $element->type->handle;
